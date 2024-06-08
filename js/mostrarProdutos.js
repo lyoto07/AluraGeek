f877616f7fc66b4bee9c4c5ba485d9fcc4c70f28
lyoto07 <guilherme-lira@outlook.com.br>
2024-06-07 23:35:49 -0300

    fix: ajuste na apresnetação do preço

--- js/mostrarProdutos.js
+++ js/mostrarProdutos.js
@@ -6,7 +6,6 @@ const lista  = document.querySelector("[data-lista]");
 function constroiCard(id,nome, preco, imagem){
     const produto = document.createElement("li");
  
-    // console.log(nome, preco, imagem)
     produto.className = `card__produto`;
     produto.dataset.id = id;
     produto.innerHTML= 
@@ -16,7 +15,7 @@ function constroiCard(id,nome, preco, imagem){
         <h2 class="card__produto__titulo">${nome}</h2>
         <div class="card__produto__conteudo">
             <p class="card-produto-price">${preco}</p>
-            // <img src="img/lixeira.png" alt="" class="delete-btn">
+            <img src="img/lixeira.png" alt="" class="delete-btn">
         </div>
     </div>
     `
